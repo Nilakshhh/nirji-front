f8b4d57d7a0b4d0bb6986da369f90899fc252d4c
Nilakshhh <dnilaksh05@gmail.com>
2024-11-03 18:02:26 +0530

    changes for deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import AllUsers from "@/components/AllUsers";
-import Navbar from "@/components/Navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,6 @@
 "use client";
 import Cookies from 'js-cookie';
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar: React.FC = () => {
@@ -20,9 +21,9 @@ const Navbar: React.FC = () => {
         "
       >
        <div className='w-[2vw] h-[2vh]'>
-          <a href="/">
+          <Link href="/">
           <svg fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><g clip-path="url(#a)"><path fill-rule="evenodd" clip-rule="evenodd" d="M10.27 14.1a6.5 6.5 0 0 0 3.67-3.45q-1.24.21-2.7.34-.31 1.83-.97 3.1M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m.48-1.52a7 7 0 0 1-.96 0H7.5a4 4 0 0 1-.84-1.32q-.38-.89-.63-2.08a40 40 0 0 0 3.92 0q-.25 1.2-.63 2.08a4 4 0 0 1-.84 1.31zm2.94-4.76q1.66-.15 2.95-.43a7 7 0 0 0 0-2.58q-1.3-.27-2.95-.43a18 18 0 0 1 0 3.44m-1.27-3.54a17 17 0 0 1 0 3.64 39 39 0 0 1-4.3 0 17 17 0 0 1 0-3.64 39 39 0 0 1 4.3 0m1.1-1.17q1.45.13 2.69.34a6.5 6.5 0 0 0-3.67-3.44q.65 1.26.98 3.1M8.48 1.5l.01.02q.41.37.84 1.31.38.89.63 2.08a40 40 0 0 0-3.92 0q.25-1.2.63-2.08a4 4 0 0 1 .85-1.32 7 7 0 0 1 .96 0m-2.75.4a6.5 6.5 0 0 0-3.67 3.44 29 29 0 0 1 2.7-.34q.31-1.83.97-3.1M4.58 6.28q-1.66.16-2.95.43a7 7 0 0 0 0 2.58q1.3.27 2.95.43a18 18 0 0 1 0-3.44m.17 4.71q-1.45-.12-2.69-.34a6.5 6.5 0 0 0 3.67 3.44q-.65-1.27-.98-3.1" fill="#666"/></g><defs><clipPath id="a"><path fill="#fff" d="M0 0h16v16H0z"/></clipPath></defs></svg>
-          </a>
+          </Link>
         </div>
        
          <svg
@@ -51,31 +52,27 @@ const Navbar: React.FC = () => {
               md:pt-0"
           >
             <li>
-              <a className="md:p-4 py-2 block hover:text-purple-400" href="#"
-                >Features</a
-              >
+              <Link className="md:p-4 py-2 block hover:text-purple-400" href="#"
+                >Features
+                </Link>
             </li>
             <li>
-              <a className="md:p-4 py-2 block hover:text-purple-400" href="#"
-                >Pricing</a
-              >
+              <Link className="md:p-4 py-2 block hover:text-purple-400" href="#"
+                >Pricing</Link>
             </li>
             <li>
-              <a className="md:p-4 py-2 block hover:text-purple-400" href="#"
-                >Blog</a
-              >
+              <Link className="md:p-4 py-2 block hover:text-purple-400" href="#"
+                >Blog</Link>
             </li>
             {isLoggedIn && <li>
-              <a className="md:p-4 py-2 block hover:text-purple-400" href="/my-profile"
-                >My Profile</a
-              >
+              <Link className="md:p-4 py-2 block hover:text-purple-400" href="/my-profile"
+                >My Profile</Link>
             </li>}
             {!isLoggedIn && <li>
-              <a
+              <Link
                 className="md:p-4 py-2 block hover:text-purple-400 text-purple-500"
                 href="/signin"
-                >Sign Up</a
-              >
+                >Sign Up</Link>
             </li>}
 
             
